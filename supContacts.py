81c999172089cc6a4c4cdb67c6cc1093668b1cb0
Benjamin Pote <ben.pote@gmail.com>
2014-10-16 20:37:59 -0400

    Derpy formatting

--- supContacts.py
+++ supContacts.py
@@ -2,17 +2,10 @@
 
 import sys
 
-
-
-
-
-
 handle = sys.argv[1]
 
 with open(handle) as myfile:
     rawContacts = myfile.read().split('\n')
-
-
 
 def supFormat(line):
     """
@@ -23,18 +16,12 @@ def supFormat(line):
     email = ''.join([x for x in elements[1].split(' ') if '@' in x])
     return name + ': ' + name + ' ' + '<' + email + '>' + '\n'
 
-    
-
-
-
-
-
 betterContacts = ""
 
 for i in range(1,len(rawContacts)-1):
     line = rawContacts[i]
     if '@' in line:
-        if ';' in line:
+        if ';' in line:  #this section deals with contacts having multiple emails
             emails = line.split(';')
             weird_ones = emails[1:]
             betterContacts += supFormat(emails[0])
@@ -45,7 +32,6 @@ for i in range(1,len(rawContacts)-1):
                 betterContacts += entry
         else:
             betterContacts += supFormat(line)
-
 
 with open("contacts.txt",'w') as writefile:
     writefile.write(betterContacts)
